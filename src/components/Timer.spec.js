f629191d8c75c9d9c3483d0529881679ec082a67
Victor Borràs <vborrasc@gmail.com>
2020-08-26 07:14:58 +0200

    change to working with numbers instead of dates

--- src/components/Timer.spec.js
+++ src/components/Timer.spec.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Timer from './timer';
 import {render} from '@testing-library/react';
 
-test('loads 00:00 as the initial value', () => {
+test('loads 25:00 as the initial value', () => {
   const {getByTestId} = render(<Timer/>);
   const timer = getByTestId('timer');
-  expect(timer).toHaveTextContent('00:00')
+  expect(timer).toHaveTextContent('25:00')
 });
--- src/components/timer.js
+++ src/components/timer.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import {useState} from 'react';
-import differenceInMinutes from 'date-fns/differenceInMinutes'
-import differenceInSeconds from 'date-fns/differenceInSeconds'
 
-function Timer () {
-  const currentTime = new Date()
+function Timer() {
+  const delta = 25 * 60;
 
-  const [endTime, setEndTime] = useState(currentTime);
-
-  const secondsDifference = differenceInSeconds(endTime, currentTime).toString().padStart(2, '0')
-  const minutesDifference = differenceInMinutes(endTime, currentTime).toString().padStart(2, '0')
+  const secondsDifference = (delta % 60).toString().padStart(2, '0');
+  const minutesDifference = (parseInt(delta / 60)).toString().
+      padStart(2, '0');
 
   return (
       <h1 data-testid="timer">{minutesDifference}:{secondsDifference}</h1>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
