0d58602ad68323c62dc5f1f2e9c215625efb21be
Victor Borràs <vborrasc@gmail.com>
2020-09-11 07:45:02 +0200

    fix tests

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-export const Button = ({children, onClick, className = ''}) => {
+export const Button = ({children, onClick, className = '', ...rest}) => {
   return (
-      <button onClick={onClick} className={className + ' ' + styles.button}>
+      <button onClick={onClick} className={className + ' ' + styles.button} {...rest}>
         {children}
       </button>
   );
