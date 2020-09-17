b0b3960f7953673f420806fbc4ddf521c9d937bf
Victor Borràs <vborrasc@gmail.com>
2020-09-17 07:25:57 +0200

    add font awesome icons

--- src/components/control-buttons/ControlButtons.js
+++ src/components/control-buttons/ControlButtons.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import {Button} from '../button/Button';
 import buttonStyles from '../button/Button.module.css';
+import styles from './ControlButtons.module.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlay, faPause, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {library} from '@fortawesome/fontawesome-svg-core';
 
-const ControlButtons = ({ isActive, setActive, setInactive, skip}) => {
+library.add(faPlay, faPause, faTimes);
+
+const ControlButtons = ({isActive, setActive, setInactive, skip}) => {
   return (
       <>
         {isActive && <Button data-testid="pause-button" onClick={setInactive}>
           Pause
+          <FontAwesomeIcon icon="pause" className={styles.icon} />
         </Button>}
-        {!isActive && <Button data-testid="activation-button" onClick={setActive}>
+        {!isActive &&
+        <Button data-testid="activation-button" onClick={setActive}>
           Start
+          <FontAwesomeIcon icon="play" className={styles.icon} />
         </Button>}
-        <Button data-testid="skip-button" className={buttonStyles['button--outline']}
-                onClick={skip}>Skip</Button>
+        <Button data-testid="skip-button"
+                className={buttonStyles['button--outline']}
+                onClick={skip}>
+          Skip
+          <FontAwesomeIcon icon="times" className={styles.icon} />
+        </Button>
       </>
-  )
-}
+  );
+};
 
-export default ControlButtons
+export default ControlButtons;
